f59c008a24253a818aceb1d4f34c429755fa4e93
Tryggvi Gylfason <tryggvigy@gmail.com>
2016-07-20 22:57:14 +0000

    Add a generic 404 page

--- src/client/common/components/Routes.js
+++ src/client/common/components/Routes.js
@@ -6,6 +6,7 @@ import LoginPage from 'client/pages/login/page'
 import HomePage from 'client/pages/home/page'
 import PingPage from 'client/pages/ping/page'
 import Terminal from 'client/pages/terminal'
+import GenericNotFound from 'client/pages/404'
 
 
 export default (
@@ -14,5 +15,6 @@ export default (
     <Route path="home" component={HomePage} />
     <Route path="terminal" component={Terminal} />
     <Route path="ping" component={PingPage} />
+    <Route path="*" component={GenericNotFound} />
   </Route>
 )
---
+++ src/client/pages/404/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './index.css'
+
+const GenericNotFound = () => (
+  <main className={styles.container}>
+    <h1 className={styles.header}>404</h1>
+    <p>There is nothing here!</p>
+    <p>༼ ༎ຶ ෴ ༎ຶ༽</p>
+  </main>
+)
+
+export default GenericNotFound
