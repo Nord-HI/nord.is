a55c4d30af95e1cb73cc06c0786da710b0a95f91
Tryggvi Gylfason <tryggvigy@gmail.com>
2016-07-23 17:43:33 +0000

    User `c` for logging

--- src/server/index.js
+++ src/server/index.js
@@ -1,8 +1,10 @@
 import app from './server'
 import config from './config'
 
+global.c = console
+
 const server = app(config)
 server.listen(
   config.PORT,
-  () => console.log(`Nord server listening at http://localhost:${config.PORT}`)
+  () => c.log(`Nord server listening at http://localhost:${config.PORT}`)
 )
