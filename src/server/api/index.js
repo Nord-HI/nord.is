49fe88cf06ff19e341920840e148bfd416f0cf50
Tryggvi Gylfason <tryggvigy@gmail.com>
2016-11-19 17:52:42 +0000

    Add logout endpoint

--- src/server/api/index.js
+++ src/server/api/index.js
@@ -2,14 +2,16 @@ import Router from 'koa-router'
 import graphqlServer from 'server/graphql'
 import loginTerminal from './loginTerminal'
 import login from './login'
+import logout from './logout'
 
 export default function api() {
   const router = new Router({ prefix: '/api' })
 
   router
-    .get('/ping', async ctx => { ctx.body = 'pong' })
+    .get('/healthy', async ctx => { ctx.body = 'ok' })
     .all('/graphql', graphqlServer)
     .post('/login', login)
+    .get('/logout', logout)
     .use(loginTerminal().routes())
 
   return router
